f5baf90bf1a1d1e755ac5584723f5dce5b94aedf
guoxuan <13702830318@163.com>
2017-04-21 16:31:21 +0800

    会议详情界面UI

---
+++ YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/activity/MeetingDetailsActivity.java
@@ -0,0 +1,41 @@
+package yy.doctor.activity;
+
+import android.support.annotation.NonNull;
+
+import lib.ys.activity.ActivityEx;
+import yy.doctor.R;
+
+/**
+ * 会议详情界面
+ *
+ * 日期 : 2017/4/21
+ * 创建人 : guoxuan
+ */
+
+public class MeetingDetailsActivity extends ActivityEx {
+    @Override
+    public void initData() {
+
+    }
+
+    @NonNull
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_meeting_details;
+    }
+
+    @Override
+    public void initTitleBar() {
+
+    }
+
+    @Override
+    public void findViews() {
+
+    }
+
+    @Override
+    public void setViewsValue() {
+
+    }
+}
