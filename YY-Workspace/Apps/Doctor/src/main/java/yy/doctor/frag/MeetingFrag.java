059fe0402b55774a812168839094c0c211067fc2
guoxuan <13702830318@163.com>
2017-04-25 17:33:15 +0800

    会议界面实现选择科室功能

--- YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/frag/MeetingFrag.java
+++ YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/frag/MeetingFrag.java
@@ -1,8 +1,10 @@
 package yy.doctor.frag;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -14,16 +16,22 @@ import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.PopupWindow.OnDismissListener;
 import android.widget.TextView;
 
 import lib.ys.LogMgr;
 import lib.ys.ex.NavBar;
+import lib.ys.util.UIUtil;
 import lib.ys.util.view.LayoutUtil;
+import lib.ys.view.DividerGridItemDecoration;
+import lib.ys.view.DividerGridItemDecoration.Decoration;
 import lib.ys.view.pager.indicator.PageIndicator;
 import lib.ys.view.pager.indicator.UnderlinePageIndicator;
 import lib.yy.frag.base.BaseVPFrag;
 import yy.doctor.R;
-import yy.doctor.adapter.SectionApadter;
+import yy.doctor.adapter.SectionAdapter;
+import yy.doctor.adapter.SectionAdapter.OnItemClickListener;
+import yy.doctor.adapter.SectionAdapter.ViewHolder;
 import yy.doctor.frag.meeting.ProgressingMeetingsFrag;
 import yy.doctor.frag.meeting.ReviewMeetingsFrag;
 import yy.doctor.frag.meeting.WaitMeetingsFrag;
@@ -35,10 +43,34 @@ import yy.doctor.frag.meeting.WaitMeetingsFrag;
  * @since 2017/4/6
  */
 public class MeetingFrag extends BaseVPFrag {
+
+    private static final int KPopupWindowBackground = R.color.main_text_bg;
     private static final int KIndicatorColor = Color.parseColor("#006ebd");
-    private static final int KIndicatorWidth = 30;
-    private static final int KGridViewRowCount = 3;
-    private View mSections;
+    private static final int KIndicatorWidth = 30;//滑块的宽度
+    private static final int KGridViewRowCount = 3;//列数
+    private static final long KDuration = 100l;//切换科室动画时长
+    private static final float KDividerHeight = 14.0f;//分割线高度
+
+    //科室选择
+    private ImageView mIvSection;
+    private TextView mTvSection;
+
+    //PopupWindow
+    private View mSections;//内容
+    private PopupWindow mPopupWindow;
+    private Drawable mPopupWindowBackground;//背景
+
+    //选择科室前的动画,箭头向上
+    private Animation mAnimDown;
+    //选择科室后的动画,箭头向下
+    private Animation mAnimUp;
+
+    private ViewGroup mLayoutTab;
+    private UnderlinePageIndicator mIndicator;
+
+    private View mPreTab;
+    private OnClickListener mTabListener;
+
 
     @IntDef({
             PageType.progressing,
@@ -51,37 +83,17 @@ public class MeetingFrag extends BaseVPFrag {
         int review = 2;//精彩回顾
     }
 
-    private static long KDuration = 100l;
-
-    //科室选择
-    private ImageView mIvSection;
-    private TextView mTvSection;
-
-    //选择科室前的动画
-    private Animation mAnimUp;
-    //选择科室后的动画
-    private Animation mAnimDown;
-
-    //是否选择过科室,false箭头向下,true箭头向上
-    private boolean mIsSelected = false;
-
-    private ViewGroup mLayoutTab;
-    private UnderlinePageIndicator mIndicator;
-
-    private View mPreTab;
-    private OnClickListener mTabListener;
-
-    private PopupWindow popupWindow;
-
     @Override
     public void initData() {
-        mAnimDown = new RotateAnimation(0.0f, 180.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        mAnimDown.setDuration(KDuration);
-        mAnimDown.setFillAfter(true);
+        mPopupWindowBackground = ContextCompat.getDrawable(getContext(), KPopupWindowBackground);
 
-        mAnimUp = new RotateAnimation(180.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        mAnimUp = new RotateAnimation(0.0f, 180.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         mAnimUp.setDuration(KDuration);
         mAnimUp.setFillAfter(true);
+
+        mAnimDown = new RotateAnimation(180.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        mAnimDown.setDuration(KDuration);
+        mAnimDown.setFillAfter(true);
 
         add(new ProgressingMeetingsFrag());
         add(new WaitMeetingsFrag());
@@ -103,8 +115,7 @@ public class MeetingFrag extends BaseVPFrag {
 
             @Override
             public void onClick(View v) {
-                mIvSection.startAnimation(mIsSelected ? mAnimUp : mAnimDown);
-                mIsSelected = !mIsSelected;
+                mIvSection.startAnimation(mAnimUp);
                 showSection(bar);
             }
         });
@@ -113,7 +124,7 @@ public class MeetingFrag extends BaseVPFrag {
             @Override
             public void onClick(View v) {
                 //TODO 跳转搜索界面
-                LogMgr.e(TAG,"搜索");
+                LogMgr.e(TAG, "搜索");
             }
         });
     }
@@ -124,22 +135,47 @@ public class MeetingFrag extends BaseVPFrag {
     private void showSection(View parent) {
         if (mSections == null) {
             mSections = inflate(R.layout.layout_meeting_select_section);
-            RecyclerView section = (RecyclerView) mSections.findViewById(R.id.meeting_layout_recyclerview);
-            section.setLayoutManager(new StaggeredGridLayoutManager(KGridViewRowCount, StaggeredGridLayoutManager.VERTICAL));
-            section.setAdapter(new SectionApadter());
-            section.addItemDecoration(new MyDecoration(getContext()));
+            RecyclerView section =
+                    (RecyclerView) mSections.findViewById(R.id.meeting_layout_recyclerview);
+            section.setLayoutManager(new StaggeredGridLayoutManager(KGridViewRowCount,
+                    StaggeredGridLayoutManager.VERTICAL));
+            section.addItemDecoration(new DividerGridItemDecoration(getContext(),
+                    Decoration.vertical, UIUtil.dpToPx(KDividerHeight, getContext()), R.drawable.divider_bg));
+            section.setAdapter(new SectionAdapter(new OnItemClickListener() {
+                @Override
+                public void onItemClick(ViewHolder viewHolder) {
+                    if (mPopupWindow != null) {
+                        mPopupWindow.dismiss();
+                        mTvSection.setText(viewHolder.getText());
+                    }
+                }
+            }));
+            mSections.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mPopupWindow != null) {
+                        mPopupWindow.dismiss();
+                    }
+                }
+            });
         }
-        if (popupWindow == null ) {
-            popupWindow = new PopupWindow();
-            popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
-            popupWindow.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
-            popupWindow.setOutsideTouchable(true);
-            popupWindow.setTouchable(true);
-            popupWindow.setFocusable(true);
-            popupWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.home_meeting_tv_major_bg));
-            popupWindow.setContentView(mSections);
+        if (mPopupWindow == null) {
+            mPopupWindow = new PopupWindow();
+            mPopupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+            mPopupWindow.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+            mPopupWindow.setOutsideTouchable(true);
+            mPopupWindow.setTouchable(true);
+            mPopupWindow.setFocusable(true);
+            mPopupWindow.setBackgroundDrawable(mPopupWindowBackground);
+            mPopupWindow.setContentView(mSections);
+            mPopupWindow.setOnDismissListener(new OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    mIvSection.startAnimation(mAnimDown);
+                }
+            });
         }
-        popupWindow.showAsDropDown(parent, 0, 0);
+        mPopupWindow.showAsDropDown(parent, 0, 0);
     }
 
     @Override
@@ -227,5 +263,15 @@ public class MeetingFrag extends BaseVPFrag {
 
         mPreTab = v;
         mPreTab.setSelected(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mPopupWindow != null) {
+            mPopupWindow.dismiss();
+            mPopupWindow = null;
+        }
     }
 }
