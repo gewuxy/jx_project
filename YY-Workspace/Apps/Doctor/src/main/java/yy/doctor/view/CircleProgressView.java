cd447da7b67d67bdbdbe66913a93ff3e14db2f9e
805717671@qq.com <w3991019>
2017-06-03 11:40:21 +0800

    1.临时提交

--- YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/view/CircleProgressView.java
+++ YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/view/CircleProgressView.java
@@ -1,6 +1,7 @@
 package yy.doctor.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -17,29 +18,30 @@ public class CircleProgressView extends View {
 
     private static final String TAG = "CircleProgressBar";
 
-    private int mMaxProgress = 100;
-
-    private int mProgress = 30;
-
     private final int mCircleLineStrokeWidth = DpFitter.dp(5);
 
     // 画圆所在的距形区域
     private final RectF mRectF;
-
     private final Paint mPaint;
 
-    private final Context mContext;
+    private int mProgress = 30;
+    private int mMaxProgress = 100;
 
     public CircleProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        mContext = context;
         mRectF = new RectF();
         mPaint = new Paint();
+
+        TypedArray ta = context.obtainStyledAttributes(attrs,R.styleable.CircleProgressView);
+        int color = ta.getColor(R.styleable.CircleProgressView_backProgress, ResLoader.getColor(R.color.divider));
+        int width = ta.getInt(R.styleable.CircleProgressView_width, mCircleLineStrokeWidth);
+        ta.recycle();
+
         // 设置画笔相关属性
         mPaint.setAntiAlias(true);
-        mPaint.setColor(ResLoader.getColor(R.color.divider));
-        mPaint.setStrokeWidth(mCircleLineStrokeWidth);
+        mPaint.setColor(color);
+        mPaint.setStrokeWidth(width);
         mPaint.setStyle(Style.STROKE);
     }
 
@@ -47,9 +49,7 @@ public class CircleProgressView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        int width = getMeasuredWidth();
-        int height = getMeasuredHeight();
-        int min = Math.min(width, height);
+        int min = Math.min(getMeasuredWidth(), getMeasuredHeight());
 
         // 位置
         mRectF.left = mCircleLineStrokeWidth / 2; // 左上角x
@@ -70,17 +70,17 @@ public class CircleProgressView extends View {
         canvas.drawArc(mRectF, -90, ((float) mProgress / mMaxProgress) * 360, false, mPaint);
     }
 
+    public void setMaxProgress(int maxProgress) {
+        mMaxProgress = maxProgress;
+    }
+
     public int getMaxProgress() {
         return mMaxProgress;
     }
 
-    public void setMaxProgress(int maxProgress) {
-        this.mMaxProgress = maxProgress;
-    }
-
     public void setProgress(int progress) {
-        this.mProgress = progress;
-        this.invalidate();
+        mProgress = progress;
+        invalidate();
     }
 
 }
