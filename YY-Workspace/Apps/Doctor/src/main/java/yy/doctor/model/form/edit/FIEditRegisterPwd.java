c61057a0d17c9bad4165bd477745b2a9484aef5f
yuansui <forgetman@qq.com>
2017-06-09 18:55:49 +0800

    修复注册的时候密码输入不能隐藏的bug

--- YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/model/form/edit/FIEditRegisterPwd.java
+++ YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/model/form/edit/FIEditRegisterPwd.java
@@ -3,6 +3,7 @@ package yy.doctor.model.form.edit;
 import android.support.annotation.NonNull;
 import android.text.InputType;
 import android.text.method.NumberKeyListener;
+import android.text.method.PasswordTransformationMethod;
 
 import lib.yy.adapter.VH.FormItemVH;
 import yy.doctor.model.form.FormType;
@@ -20,10 +21,10 @@ public class FIEditRegisterPwd extends FIEditRegister {
     }
 
     @Override
-    protected void refresh(FormItemVH holder) {
-        super.refresh(holder);
+    protected void init(FormItemVH holder) {
+        super.init(holder);
 
-        holder.getEt().setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        // 设置输入digits
         holder.getEt().setKeyListener(new NumberKeyListener() {
 
             @Override
@@ -37,5 +38,8 @@ public class FIEditRegisterPwd extends FIEditRegister {
                 return InputType.TYPE_TEXT_VARIATION_PASSWORD;
             }
         });
+
+        // FIXME: 由于上面指定了key listener, 导致input type的text隐藏失效, 所以手动设置为密码隐藏模式
+        holder.getEt().setTransformationMethod(PasswordTransformationMethod.getInstance());
     }
 }
