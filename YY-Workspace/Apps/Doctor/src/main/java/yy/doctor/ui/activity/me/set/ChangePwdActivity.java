6c9c62be2823fb3a35351f72558a0541b4d37b70
805717671@qq.com <w3991019>
2017-07-24 20:04:20 +0800

    1.修改密码UI

--- YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/ui/activity/me/set/ChangePwdActivity.java
+++ YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/ui/activity/me/set/ChangePwdActivity.java
@@ -1,16 +1,66 @@
 package yy.doctor.ui.activity.me.set;
 
+import android.support.annotation.IntDef;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import lib.ys.util.TextUtil;
+import yy.doctor.model.form.Builder;
+import yy.doctor.model.form.FormType;
+import yy.doctor.util.Util;
+
 /**
  * @auther : GuoXuan
  * @since : 2017/7/24
  */
-public class ChangePwdActivity extends BaseSetActivity {
+public class ChangePwdActivity extends BaseSetActivity implements TextWatcher {
+
+    private EditText mEtPwdOld;
+    private EditText mEtPwdNew;
+
+    @IntDef({
+            RelatedId.pwd_old,
+            RelatedId.pwd_new,
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    private @interface RelatedId {
+        int pwd_old = 0;
+        int pwd_new = 1;
+    }
 
     @Override
     public void initData() {
         super.initData();
 
+        addItem(new Builder(FormType.et_register_pwd)
+                .related(RelatedId.pwd_old)
+                .hint("请输入旧密码")
+                .build());
 
+        addItem(new Builder(FormType.divider)
+                .build());
+
+        addItem(new Builder(FormType.et_register_pwd)
+                .related(RelatedId.pwd_new)
+                .hint("请输入新密码")
+                .build());
+
+        addItem(new Builder(FormType.divider)
+                .build());
+    }
+
+    @Override
+    public void setViews() {
+        super.setViews();
+
+        mEtPwdOld = getRelatedItem(RelatedId.pwd_old).getHolder().getEt();
+        mEtPwdNew = getRelatedItem(RelatedId.pwd_new).getHolder().getEt();
+        mEtPwdOld.addTextChangedListener(this);
+        mEtPwdNew.addTextChangedListener(this);
     }
 
     @Override
@@ -21,5 +71,20 @@ public class ChangePwdActivity extends BaseSetActivity {
     @Override
     public CharSequence getSetText() {
         return "确认并登录";
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        setChanged(TextUtil.isNotEmpty(Util.getEtString(mEtPwdOld))
+                && TextUtil.isNotEmpty(Util.getEtString(mEtPwdNew)));
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
     }
 }
