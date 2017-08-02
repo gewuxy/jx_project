990f2924fbb052415973b58678435d68f22e9baa
yuansui <forgetman@qq.com>
2017-08-02 18:16:17 +0800

    AutoBuilder加入类型区分判断

--- Lib-Workspace/LibAnnotation/src/main/java/lib/annotation/processor/AutoBuilderProcessor.java
+++ Lib-Workspace/LibAnnotation/src/main/java/lib/annotation/processor/AutoBuilderProcessor.java
@@ -1,7 +1,9 @@
 package lib.annotation.processor;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
@@ -11,14 +13,19 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import javax.annotation.processing.Processor;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.ElementFilter;
+import javax.tools.Diagnostic.Kind;
 
 import lib.annotation.AutoBuilder;
+import lib.annotation.Extra;
 import lib.annotation.Ignore;
+
+import static java.nio.file.Paths.get;
 
 
 /**
@@ -56,7 +63,13 @@ public class AutoBuilderProcessor extends BaseProcessor {
                 }
 
                 String paramName = getParamName(field, field.getSimpleName().toString());
-                builder.addField(TypeName.get(field.asType()), paramName, Modifier.PRIVATE);
+
+                FieldSpec.Builder fieldBuilder = FieldSpec.builder(TypeName.get(field.asType()), paramName, Modifier.PRIVATE);
+//                for (AnnotationMirror annotation : field.getAnnotationMirrors()) {
+//                    getMessager().printMessage(Kind.NOTE, annotation.getAnnotationType().asElement().getSimpleName().toString());
+//                    fieldBuilder.addAnnotation(ClassName.bestGuess(annotation.getAnnotationType().asElement().getSimpleName().toString()));
+//                }
+                builder.addField(fieldBuilder.build());
 
                 MethodSpec spec = MethodSpec.methodBuilder(paramName)
                         .addModifiers(Modifier.PUBLIC)
@@ -90,9 +103,31 @@ public class AutoBuilderProcessor extends BaseProcessor {
                 String filedName = field.getSimpleName().toString();
                 String paramName = getParamName(field, filedName);
 
+                TypeName typeName = TypeName.get(field.asType());
+                if (typeName.isBoxedPrimitive()) {
+                    // 基础类型Integer Long等
+                    buildMethod.beginControlFlow("if (this.$N != null)", paramName);
+                } else {
+                    // 基础类型int long等
+                    if (typeName == TypeName.LONG) {
+                        buildMethod.beginControlFlow("if (this.$N != 0)", paramName);
+                    } else if (typeName == TypeName.INT) {
+                        buildMethod.beginControlFlow("if (this.$N != 0)", paramName);
+                    } else if (typeName == TypeName.FLOAT) {
+                        buildMethod.beginControlFlow("if (this.$N != 0)", paramName);
+                    } else if (typeName == TypeName.DOUBLE) {
+                        buildMethod.beginControlFlow("if (this.$N != 0)", paramName);
+                    } else if (typeName == TypeName.BOOLEAN) {
+                        buildMethod.beginControlFlow("if (!this.$N)", paramName);
+                    } else {
+                        buildMethod.beginControlFlow("if (this.$N != null)", paramName);
+                    }
+                }
+
                 buildMethod.addStatement("$T $N = $T.class.getDeclaredField(\"$N\")", Field.class, paramName, annotatedTypeName, filedName)
                         .addStatement("$N.setAccessible(true)", paramName)
-                        .addStatement("$N.set(inst, this.$N)", paramName, paramName);
+                        .addStatement("$N.set(inst, this.$N)", paramName, paramName)
+                        .endControlFlow();
             }
 
             buildMethod.nextControlFlow("catch (Exception e)")
