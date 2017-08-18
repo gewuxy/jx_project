c84c8e478ccd0777e13baebb575cd7115e41f310
yuansui <forgetman@qq.com>
2017-08-18 17:02:05 +0800

    1. DiCompile名字错误->DI-Compiler 2. 调整App里nav bar初始化相关 3. 网络初始化移动到App里 4. 修改文件后缀定义位置

--- DICompile/src/main/java/inject/android/AnnotationClassName.java
+++
@@ -1,7 +0,0 @@
-package inject.android;
-
-import com.squareup.javapoet.ClassName;
-
-public interface AnnotationClassName {
-    ClassName KNonNull = ClassName.get("android.support.annotation", "NonNull");
-}
