cf14f3abc79150c5e474c84737609de0ee8ec6df
805717671@qq.com <w3991019>
2017-08-21 18:45:55 +0800

    1.修改PopupWindowEx 2.科室筛选的无网操作

--- YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/popup/SectionPopup.java
+++ YY-Workspace/Apps/Doctor/src/main/java/yy/doctor/popup/SectionPopup.java
@@ -14,6 +14,9 @@ import java.util.List;
 
 import lib.network.model.NetworkError;
 import lib.network.model.NetworkResp;
+import lib.ys.config.AppConfig.RefreshWay;
+import lib.ys.ui.decor.DecorViewEx.ViewState;
+import lib.ys.ui.interfaces.listener.OnRetryClickListener;
 import lib.ys.ui.other.PopupWindowEx;
 import lib.yy.network.ListResult;
 import yy.doctor.R;
@@ -22,19 +25,23 @@ import yy.doctor.model.meet.MeetingDepartment;
 import yy.doctor.model.meet.MeetingDepartment.TMeetingDepartment;
 import yy.doctor.network.JsonParser;
 import yy.doctor.network.NetworkAPISetter.MeetAPI;
+import yy.doctor.util.Util;
 
 /**
  * @auther yuansui
  * @since 2017/4/26
  */
 
-public class SectionPopup extends PopupWindowEx implements OnItemClickListener {
-
+public class SectionPopup extends PopupWindowEx implements OnItemClickListener, OnRetryClickListener {
 
     private OnSectionListener mLsn;
 
     private ListView mListView;
     private MeetingDepartmentAdapter adapter;
+
+    public interface OnSectionListener {
+        void onSectionSelected(String text);
+    }
 
     public SectionPopup(@NonNull Context context, @Nullable OnSectionListener l) {
         super(context);
@@ -58,10 +65,14 @@ public class SectionPopup extends PopupWindowEx implements OnItemClickListener {
         mListView = findView(R.id.section_filter_lv);
     }
 
-
     @Override
     public void setViews() {
         adapter = new MeetingDepartmentAdapter();
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        refresh(RefreshWay.embed);
         exeNetworkReq(MeetAPI.meetDepartment().build());
     }
 
@@ -91,20 +102,31 @@ public class SectionPopup extends PopupWindowEx implements OnItemClickListener {
     @Override
     public void onNetworkSuccess(int id, Object result) {
         ListResult r = (ListResult) result;
-        List<MeetingDepartment> data = r.getData();
-        adapter.setData(data);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(this);
+        if (r.isSucceed()){
+            List<MeetingDepartment> data = r.getData();
+            adapter.setData(data);
+            mListView.setAdapter(adapter);
+            mListView.setOnItemClickListener(this);
+            setViewState(ViewState.normal);
+        } else {
+            setViewState(ViewState.error);
+        }
     }
 
     @Override
     public void onNetworkError(int id, NetworkError error) {
         super.onNetworkError(id, error);
-       // setViewState(ViewState.error);
+
+        setViewState(ViewState.error);
     }
 
-    public interface OnSectionListener {
-        void onSectionSelected(String text);
+    @Override
+    public boolean onRetryClick() {
+        if (super.onRetryClick()) {
+            return true;
+        }
+        getDataFromNet();
+        return false;
     }
 
 }
