007de513c9a8f49d79b90d62b27e394b4972a9e1
forgetman@qq.com <forget850821>
2017-10-24 11:28:13 +0800

    1. 升级到studio rc-2 2. FormEx更换使用方式, 泛型增加声明子类型, 方便链式调用

--- YY-Workspace/Libs/LibYY/src/main/java/lib/yy/model/form/BaseForm.java
+++ YY-Workspace/Libs/LibYY/src/main/java/lib/yy/model/form/BaseForm.java
@@ -1,21 +1,15 @@
 package lib.yy.model.form;
 
-import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.StringRes;
 import android.text.InputFilter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
-
 import lib.ys.ConstantsEx;
 import lib.ys.form.FormEx;
-import lib.ys.form.OnFormObserver;
 import lib.ys.util.RegexUtil;
 import lib.ys.util.TextUtil;
 import lib.ys.util.UIUtil;
@@ -30,7 +24,7 @@ import lib.yy.adapter.VH.FormVH;
  * @author CaiXiang
  * @since 2017/4/5
  */
-abstract public class BaseForm extends FormEx<FormVH> {
+abstract public class BaseForm extends FormEx<BaseForm, FormVH> {
 
     private int mPaddingLeft;
     private int mPaddingRight;
@@ -207,201 +201,6 @@ abstract public class BaseForm extends FormEx<FormVH> {
         return true;
     }
 
-    @Override
-    public BaseForm name(@StringRes int id) {
-        return super.name(id);
-    }
-
-    @Override
-    public BaseForm name(String name) {
-        return super.name(name);
-    }
-
-    @Override
-    public BaseForm key(String key) {
-        return super.key(key);
-    }
-
-    @Override
-    public BaseForm val(String val) {
-        return super.val(val);
-    }
-
-    @Override
-    public BaseForm text(String text) {
-        return super.text(text);
-    }
-
-    @Override
-    public BaseForm texts(String... texts) {
-        return super.texts(texts);
-    }
-
-    @Override
-    public BaseForm texts(@StringRes int... ids) {
-        return super.texts(ids);
-    }
-
-    @Override
-    public BaseForm text(@StringRes int id) {
-        return super.text(id);
-    }
-
-    @Override
-    public BaseForm hint(String hint) {
-        return super.hint(hint);
-    }
-
-    @Override
-    public BaseForm hint(@StringRes int id) {
-        return super.hint(id);
-    }
-
-    @Override
-    public BaseForm tips(String tips) {
-        return super.tips(tips);
-    }
-
-    @Override
-    public BaseForm tips(@StringRes int id) {
-        return super.tips(id);
-    }
-
-    @Override
-    public BaseForm enable(boolean able) {
-        return super.enable(able);
-    }
-
-    @Override
-    public BaseForm related(Object related) {
-        return super.related(related);
-    }
-
-    @Override
-    public BaseForm data(Object data) {
-        return super.data(data);
-    }
-
-    @Override
-    public BaseForm option(Object option) {
-        return super.option(option);
-    }
-
-    @Override
-    public BaseForm host(Object host) {
-        return super.host(host);
-    }
-
-    @Override
-    public BaseForm width(int w) {
-        return super.width(w);
-    }
-
-    @Override
-    public BaseForm height(int h) {
-        return super.height(h);
-    }
-
-    @Override
-    public BaseForm column(int c) {
-        return super.column(c);
-    }
-
-    @Override
-    public BaseForm limit(int limit) {
-        return super.limit(limit);
-    }
-
-    @Override
-    public BaseForm background(@ColorInt int color) {
-        return super.background(color);
-    }
-
-    @Override
-    public BaseForm backgroundRes(@ColorRes int id) {
-        return super.backgroundRes(id);
-    }
-
-    @Override
-    public BaseForm mode(int mode) {
-        return super.mode(mode);
-    }
-
-    @Override
-    public BaseForm drawable(@DrawableRes int id) {
-        return super.drawable(id);
-    }
-
-    @Override
-    public BaseForm layout(@LayoutRes int id) {
-        return super.layout(id);
-    }
-
-    @Override
-    public BaseForm textColorRes(@ColorRes int id) {
-        return super.textColorRes(id);
-    }
-
-    @Override
-    public BaseForm textColor(@ColorInt int color) {
-        return super.textColor(color);
-    }
-
-    @Override
-    public BaseForm nameColor(@ColorInt int color) {
-        return super.nameColor(color);
-    }
-
-    @Override
-    public BaseForm toast(String toast) {
-        return super.toast(toast);
-    }
-
-    @Override
-    public BaseForm intent(Intent i) {
-        return super.intent(i);
-    }
-
-    @Override
-    public BaseForm children(List children) {
-        return super.children(children);
-    }
-
-    @Override
-    public BaseForm depend(Object depend) {
-        return super.depend(depend);
-    }
-
-    @Override
-    public BaseForm regex(String regex) {
-        return super.regex(regex);
-    }
-
-    @Override
-    public BaseForm check(boolean check) {
-        return super.check(check);
-    }
-
-    @Override
-    public BaseForm index(int index) {
-        return super.index(index);
-    }
-
-    @Override
-    public BaseForm id(int id) {
-        return super.id(id);
-    }
-
-    @Override
-    public BaseForm visible(boolean visible) {
-        return super.visible(visible);
-    }
-
-    @Override
-    public BaseForm observer(OnFormObserver observer) {
-        return super.observer(observer);
-    }
-
     private void observed(String val) {
         if (getObserver() != null) {
             getObserver().callback(getRelated(), TextUtil.isNotEmpty(val));
@@ -419,4 +218,9 @@ abstract public class BaseForm extends FormEx<FormVH> {
         super.save(val, text);
         observed(val);
     }
+//
+//    @Override
+//    protected BaseForm getThis() {
+//        return this;
+//    }
 }
